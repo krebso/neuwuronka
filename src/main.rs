372c5102f20a8529046655d20ad02fb93b6cd8cb
Martin Krebs <martin.krebs@kiwi.com>
2022-09-24 19:54:27 +0200

    Rewrite matrix operations

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,20 @@ use crate::matrix::Matrix;
 fn test() {
     let mut m = Matrix::<i32>::create_matrix(2, 2);
 
-    assert!(m == Matrix::<i32>::create_matrix(2, 2));
+    assert_eq!(m, Matrix::<i32>::create_matrix(2, 2));
 
     m.set(0, 0, 1);
 
-    assert!(m != Matrix::<i32>::create_matrix(2, 2));
+    assert_ne!(m, Matrix::<i32>::create_matrix(2, 2));
 
-    assert!(m.get(0, 0) == 1);
+    assert_eq!(m.get(0, 0), 1);
+
+    assert_eq!(m.clone() - m.clone(), Matrix::<i32>::create_matrix(2, 2));
+
+    let mut add = m.clone();
+    add.set(0, 0, 2);
+
+    assert_eq!( m.clone() + m.clone(), add);
 }
 
 fn main() {
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,6 +1,8 @@
 /// Module for matrix operations
 
-#[derive(Eq, PartialEq)]
+use std::ops::{Add, AddAssign, Mul, Sub};
+
+#[derive(Eq, PartialEq, Debug, Clone, Default)]
 pub struct Matrix<T> {
     width: usize,
     height: usize,
@@ -31,5 +33,75 @@ impl<T: Default + Copy> Matrix<T> {
 
     pub fn get(&self, x: usize, y: usize) -> T {
         self._matrix[y][x]
+    }
+
+}
+
+impl<T : Add<T, Output = T> + Default + Copy> Add for Matrix<T> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        if self.width != other.width || self.height != other.height {
+            panic!("Cannot add two matrices, incompatible dimensions.")
+        }
+
+        let mut sum = Matrix::create_matrix(self.width, self.height);
+
+        for y in 0..self.height {
+            for x in 0..other.width {
+                sum.set(y, x, self.get(y, x) + other.get(y, x));
+            }
+        }
+
+        sum
+    }
+}
+
+
+impl<T : Sub<T, Output = T> + Default + Copy> Sub for Matrix<T> {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        if self.width != other.width || self.height != other.height {
+            panic!("Cannot subtract two matrices, incompatible dimensions.")
+        }
+
+        let mut diff = Matrix::create_matrix(self.width, self.height);
+
+        for y in 0..self.height {
+            for x in 0..other.width {
+                diff.set(y, x, self.get(y, x) - other.get(y, x));
+            }
+        }
+
+        diff
+    }
+}
+
+
+impl<T : Mul<T, Output = T> + Add<T, Output = T> + Default + Copy + AddAssign> Mul for Matrix<T> {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self::Output {
+        if self.width != other.height {
+            panic!("Cannot multiply two matrices, incompatible dimensions.")
+        }
+
+        let mut prod = Matrix::create_matrix(self.height, other.width);
+
+        for y in 0..self.height {
+            for x in 0..self.width {
+
+                let mut sum = T::default();
+
+                for k in 0..self.height {
+                    sum += self.get(y, k) + other.get(x, k);
+                }
+
+                prod.set(y, x, sum)
+            }
+        }
+
+        prod
     }
 }
